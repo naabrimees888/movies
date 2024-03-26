ef40191a4832a7187bc87b72e4d24166aa9f0e18
naabrimees888 <43852467+naabrimees888@users.noreply.github.com>
2024-03-26 16:09:12 +0200

    Movies with posters

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { getResults } from './request';
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
   const [searchResults, setSearchResults] = useState([]);
+  const [selectedMovie, setSelectedMovie] = useState(null);
 
   const updateSearchTerm = (event) => {
     setSearchTerm(event.target.value);
@@ -13,12 +14,19 @@ function App() {
   const search = async () => {
     const data = await getResults({ search: searchTerm });
     setSearchResults(data.results);
+    setSelectedMovie(null);
   };
 
-  return (
+  const updateSelectedMovie = (movie) => {
+    setSelectedMovie(movie);
+  }
+
+  const baseURL = 'https://image.tmdb.org/t/p/w500/';
+
+  return (    
     <section>
       <div className='header'>
-      <h1>Filmide otsing</h1>
+        <h1>Filmide otsing</h1>
       </div>
       <div className="container">
         <div className='search-box'>
@@ -28,14 +36,36 @@ function App() {
           </div>
           <div className="search-results">
             <h2>Leitud filmid</h2>
-            <ul>
-              {searchResults.map((movie) => (
-                <li key={movie.id}>{movie.title}</li>
-              ))}
-            </ul>
+
+            {selectedMovie && (
+              <div className="selected-movie">
+                <h2>{selectedMovie.title} ({(new Date(selectedMovie.release_date)).getFullYear()})</h2>
+                <div className="selected-movie-image-details">
+                  <img src={`${baseURL}/${selectedMovie.poster_path}`} alt={selectedMovie.title} />
+                  <div className='selected-movie__details'>
+                    <p><strong>Ülevaade:</strong> {selectedMovie.overview}</p>
+                    <p><strong>Avadatud:</strong> {(new Date(selectedMovie.release_date)).getFullYear()}</p>
+                    <p><strong>Keskmine hinne:</strong> {selectedMovie.vote_average}</p>
+                  </div>
+                </div>
+              </div>
+            )}
+            <div className="movie-container">
+              <div className="movie-grid">
+                {searchResults.map((movie) => (
+                  <div key={movie.id} className="movie-card"  onClick={() => updateSelectedMovie(movie)}>
+                    <img src={`${baseURL}/${movie.poster_path}`} alt={movie.title} />
+                    <h3>{movie.title} ({(new Date(movie.release_date)).getFullYear()})</h3>
+                    <p className="overview"><strong>Ülevaade:</strong> {movie.overview}</p>
+                    <p><strong>Keskmine hinne:</strong> {movie.vote_average}</p>
+                  </div>
+                ))}
+              </div>
+            </div>
           </div>
-        </div>
+        </div>        
       </div>
+      <div className='footer'></div>
     </section>
   );
 }
